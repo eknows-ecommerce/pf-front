7bef3b2a98b1633f8bba7027b5081471f7c35174
MatiasCardullo <36178170+MatiasCardullo@users.noreply.github.com>
2022-08-03 16:07:15 -0300

    Empezando en Landing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Landing from './pages/Landing';
-import Home from './pages/Home';
+/*import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Error from './pages/Error'
-import Details from './pages/Details';
+import Details from './pages/Details';*/
 import { getAll } from './features/actions/categorias'
 import { useDispatch } from 'react-redux'
 import { useEffect } from 'react'
@@ -21,14 +21,16 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/books" element={<Home />} />
-          <Route path="/books/:id" element={<Details />} />
-          <Route path="/user" element={<Profile />} />
-          <Route path="*" element={<Error />} />
         </Routes>
       </div>
     </BrowserRouter>
   )
 }
+/*
+          <Route path="/books" element={<Home />} />
+          <Route path="/books/:id" element={<Details />} />
+          <Route path="/user" element={<Profile />} />
+          <Route path="*" element={<Error />} />
 
+*/
 export default App
---
+++ src/pages/Landing.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+//import s from '../styles/Landing.module.css'
+
+export default function Landing() {
+  return (
+    <div>
+      <h1>Hola!, 
+  bienvenidos a e-Knews
+</h1>
+    </div>
+  )
+}
+/*
+
+    <div className={s.landingContainer}>
+        <h1 className={s.landingTitle}>Recipe's Ideas</h1>
+        <div className={s.text}>
+            <p>Welcome!</p>
+        </div>
+
+    <div className={s.buttonContainer}>
+    <Link to= '/recipes'>
+    <button className={s.button}>Enter</button>
+    </Link>
+    </div>
+    </div>
+*/
