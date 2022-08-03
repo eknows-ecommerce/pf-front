758109dd46aaa57d46fdacbe9e50a38b6f5b121c
nnicolasg <104043118+nnicolasg@users.noreply.github.com>
2022-08-03 16:37:42 -0300

    me olvide de quitar codigo extra

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css'
 
-import PruebaCategoriasReducer from './PruebaCategoriasReducer'
-
 function App() {
   return (
     <div className="App">
       <h1> inicio</h1>
-      <PruebaCategoriasReducer />
     </div>
   )
 }
