d7945d34dce30747d904e98d28155557227331cd
MatiasCardullo <36178170+MatiasCardullo@users.noreply.github.com>
2022-08-03 19:20:15 -0300

    Tailwind instalado
    landing text
    ahora agregando imagenes

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,28 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-//import s from '../styles/Landing.module.css'
 
 export default function Landing() {
   return (
     <div>
-      <h1>Hola!, 
-  bienvenidos a e-Knews
-</h1>
+      <h1 className="text-6xl font-bold font-poiret-one p-10">
+        Hola!, bienvenidos a e-Knews
+      </h1>
+      <h2 className="text-1xl font-bold font-poiret-one">
+        donde encuentras tus historias
+      </h2>
+      <br></br>
+      <h2 className="text-2xl font-semibold font-poiret-one">
+        Viaja a otro universo con solo un click
+      </h2>
+      <br></br>
+      <h2 className="text-3xl font-semibold font-poiret-one">
+        K-views
+      </h2>
+      <h2 className="text-2xl font-poiret-one">
+        Las historias mas atrapantes, a donde quieras
+      </h2>
+      <br></br>
+      <h2 className="text-3xl font-semibold font-poiret-one">
+        K-stories
+      </h2>
+      <h2 className="text-2xl font-poiret-one">
+        Para tí, en todas las formas
+      </h2>
     </div>
   )
 }
-/*
-
-    <div className={s.landingContainer}>
-        <h1 className={s.landingTitle}>Recipe's Ideas</h1>
-        <div className={s.text}>
-            <p>Welcome!</p>
-        </div>
-
-    <div className={s.buttonContainer}>
-    <Link to= '/recipes'>
-    <button className={s.button}>Enter</button>
-    </Link>
-    </div>
-    </div>
-*/
