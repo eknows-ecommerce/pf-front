c3e003c465929cc4ce430ab2fd1134a092dc4911
MatiasCardullo <36178170+MatiasCardullo@users.noreply.github.com>
2022-08-03 21:00:22 -0300

    Login y Register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,14 +4,36 @@ import { Link } from 'react-router-dom'
 export default function Landing() {
   return (
     <div>
-      <h1 className="text 2xl font-bold font-poiret-one p-2">
-        Email:
+      <h1 className="font-bold font-poiret-one p-2">
+        Usuario:
       </h1>
-      <input type={"email"}></input>
-      <h1 className="text 2xl font-bold font-poiret-one p-2">
+      <input type={"text"} className="font-bold font-poiret-one p-1 border-2"></input>
+      <h1 className="font-bold font-poiret-one p-2">
         Contraseña:
       </h1>
-      <input type={"password"}></input>
+      <input type={"password"} className="font-bold font-poiret-one p-1 border-2"></input>
+      <div className='p-2'>
+        <Link to='/login' className='p-1'>
+          <button className='p-1 pt-0 border-2 rounded-xl'>
+            Ingresar
+          </button>
+        </Link>
+      </div>
+      <h1 className="p-2">
+        o conecta con:
+      </h1>
+      <div className='p-2'>
+        <Link to='/login' className='p-1'>
+          <button className='p-1 pt-0 border-2 rounded-xl'>
+            Google img
+          </button>
+        </Link>
+        <Link to='/login' className='p-1'>
+          <button className='p-1 pt-0 border-2 rounded-xl'>
+            Github img
+          </button>
+        </Link>
+      </div>
     </div>
   )
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,6 +4,40 @@ import { Link } from 'react-router-dom'
 export default function Landing() {
   return (
     <div>
+      <h1 className="font-bold font-poiret-one p-2">
+        Email:
+      </h1>
+      <input type={"email"} className="font-bold font-poiret-one p-1 border-2"></input>
+      <h1 className="font-bold font-poiret-one p-2">
+        Contraseña:
+      </h1>
+      <input type={"password"} className="font-bold font-poiret-one p-1 border-2"></input>
+      <h1 className="font-bold font-poiret-one p-2">
+        Confirme contraseña:
+      </h1>
+      <input type={"password"} className="font-bold font-poiret-one p-1 border-2"></input>
+      <div className='p-2'>
+        <Link to='/register' className='p-1'>
+          <button className='p-1 pt-0 border-2 rounded-xl'>
+            Registrarse
+          </button>
+        </Link>
+      </div>
+      <h1 className="p-2">
+        o usa la cuenta de:
+      </h1>
+      <div className='p-2'>
+        <Link to='/login' className='p-1'>
+          <button className='p-1 pt-0 border-2 rounded-xl'>
+            Google img
+          </button>
+        </Link>
+        <Link to='/login' className='p-1'>
+          <button className='p-1 pt-0 border-2 rounded-xl'>
+            Github img
+          </button>
+        </Link>
+      </div>
     </div>
   )
 }
