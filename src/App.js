153dd947a05002dff8004905225800523965c0b4
Alberto Ruben Echavarria Zalazar <94318456+alberto012@users.noreply.github.com>
2022-08-03 21:48:31 -0300

    creacion del footer sin funcionalidades

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
     <div className="App">
       <h1> inicio</h1>
+      <Footer />
     </div>
   )
 }
