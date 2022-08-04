1a12d94aabac5a0d70755ae29f4758a1c28c99ca
Alberto Ruben Echavarria Zalazar <94318456+alberto012@users.noreply.github.com>
2022-08-03 22:55:35 -0300

    navbar creada

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css'
+import Navbar from './components/NavBar/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1> inicio</h1>
     </div>
   )
