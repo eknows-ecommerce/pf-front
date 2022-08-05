be58845d6db2c09aeb936214957479e86694b012
MatiasCardullo <36178170+MatiasCardullo@users.noreply.github.com>
2022-08-05 09:09:32 -0300

    Update LandingBottom.jsx

--- src/components/section/LandingBottom.jsx
+++ src/components/section/LandingBottom.jsx
@@ -4,27 +4,17 @@ import logo from './../../assets/img/logo3.png'
 
 export default function SectionSesion() {
   return (
-    <div>
-      <div className='display: flex'>
-        <img src={logo}></img>
-        <div>
-          <h1 className="text-6xl font-bold font-poiret-one p-5">
-            Hola!, bienvenidos a e-Knews
-          </h1>
-          <h2 className="text-1xl font-bold font-poiret-one p-5">
-            donde encuentras tus historias
-          </h2>
-        </div>
-      </div>
-      <div>
-        <Link to="/login" className="p-1">
-          <button className="p-1 border-2 rounded-xl">Ingresar</button>
-        </Link>
-        <Link to="/register" className="p-1">
-          <button className="p-1 border-2 rounded-xl">Registrarse</button>
-        </Link>
-      </div>
-    </div>
+    <>
+      <h2 className="text-3xl font-semibold font-poiret-one">K-views</h2>
+      <h2 className="text-2xl font-poiret-one">
+        Las historias mas atrapantes, a donde quieras
+      </h2>
+      <br></br>
+      <h2 className="text-3xl font-semibold font-poiret-one">K-stories</h2>
+      <h2 className="text-2xl font-poiret-one">
+        Para tí, en todas las formas
+      </h2>
+    </>
   )
 }
 
