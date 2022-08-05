c84cf953fd75cc2bb843ac8bd964a1a42e615d84
MatiasCardullo <36178170+MatiasCardullo@users.noreply.github.com>
2022-08-05 09:08:01 -0300

    Landing and 404 img imported

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,22 +1,14 @@
 import Carrusel from '../../components/carrusel/Carrusel'
 import Footer from '../../components/footer/Footer'
 import SectionSesion from '../../components/section/SectionSesion'
+import LandingBottom from '../../components/section/LandingBottom'
 
 export default function Landing() {
   return (
     <div>
       <SectionSesion />
       <Carrusel />
-      <br></br>
-        <h2 className="text-3xl font-semibold font-poiret-one">K-views</h2>
-        <h2 className="text-2xl font-poiret-one">
-          Las historias mas atrapantes, a donde quieras
-        </h2>
-      <br></br>
-        <h2 className="text-3xl font-semibold font-poiret-one">K-stories</h2>
-        <h2 className="text-2xl font-poiret-one">
-          Para tí, en todas las formas
-        </h2>
+      <LandingBottom />
       <Footer />
     </div>
   )
