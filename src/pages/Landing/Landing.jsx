9ec117d1a2fd65a7ac60b4667fa3a61e92fd441e
MatiasCardullo <36178170+MatiasCardullo@users.noreply.github.com>
2022-08-05 08:54:36 -0300

    Landing

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -7,6 +7,16 @@ export default function Landing() {
     <div>
       <SectionSesion />
       <Carrusel />
+      <br></br>
+        <h2 className="text-3xl font-semibold font-poiret-one">K-views</h2>
+        <h2 className="text-2xl font-poiret-one">
+          Las historias mas atrapantes, a donde quieras
+        </h2>
+      <br></br>
+        <h2 className="text-3xl font-semibold font-poiret-one">K-stories</h2>
+        <h2 className="text-2xl font-poiret-one">
+          Para tí, en todas las formas
+        </h2>
       <Footer />
     </div>
   )
