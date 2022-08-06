59528416d360c9e139b148ada8a58a309ed3c74d
AlexTRDev <104025084+AlexTRDev@users.noreply.github.com>
2022-08-06 13:13:10 -0500

    TEMPLATES - Button

--- src/components/templates/Button.jsx
+++ src/components/templates/Button.jsx
@@ -1,5 +1,13 @@
-function Button() {
-  return <button>Personlizado utilizando tailwindcss</button>
+function Button({ children, primary, secondary }) {
+  const css = primary
+    ? `
+   bg-violet-500 inline-block  px-8 py-3 text-sm font-medium text-white transition rounded hover:scale-110 hover:shadow-xl active:bg-indigo-500 focus:outline-none focus:ring
+  `
+    : `
+  inline-block px-8 py-3 text-sm font-medium text-indigo-600 transition border border-current rounded hover:scale-110 hover:shadow-xl active:text-indigo-500 focus:outline-none focus:rin
+  `
+
+  return <button className={`${css}`}>{children}</button>
 }
 
 export default Button
