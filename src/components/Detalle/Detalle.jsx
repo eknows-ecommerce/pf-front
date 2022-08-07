a9a43661ee23a32aea22fb7a29397eae87aff372
MatiasCardullo <36178170+MatiasCardullo@users.noreply.github.com>
2022-08-07 20:17:23 -0300

    Detalle colores and responsive

--- src/components/Detalle/Detalle.jsx
+++ src/components/Detalle/Detalle.jsx
@@ -6,44 +6,44 @@ import Button from '../templates/Button'
 import NavBar from '../NavBar/Navbar'
 
 function ReviewCard({ title, text, author, score }) {
-  function Stars(){
-    let out=[];let star;
+  function Stars() {
+    let out = []; let star;
     for (let i = 0; i < 5; i++) {
-      i<score?star="hover:animate-spin text-yellow-500":star="text-gray-300"
+      i < score ? star = "hover:animate-spin text-yellow-500" : star = "text-gray-300"
       out.push(
         <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className={"w-6 h-6 "+star}
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-            </svg>
-            )
+          xmlns="http://www.w3.org/2000/svg"
+          className={"w-6 h-6 " + star}
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+        </svg>
+      )
     }
 
-    return(out)
+    return (out)
   }
 
   return (
-    <blockquote className="flex flex-col justify-between h-full p-8 bg-white rounded-3xl shadow-xl
+    <blockquote className="flex flex-col justify-between h-full p-8 bg-slate-100 rounded-3xl shadow-xl
     hover:scale-x-[1.01] hover:scale-y-[1.01]">
       <div>
         <div className="flex space-x-0.5">
-            <Stars/>
-         </div>
+          <Stars />
+        </div>
         <div className="mt-4">
-          <h5 className="text-xl text-center font-bold text-pink-600 sm:text-2xl">
+          <h5 className="text-xl text-center font-bold text-pink-700 sm:text-2xl">
             {title}
           </h5>
-          <p className="mt-4 text-gray-600 text-justify">
+          <p className="mt-4 text-gray-800 text-justify">
             Lorem ipsum dolor sit amet consectetur adipisicing
             elit. Ipsam cumque recusandae dolorum porro, quasi
             sunt necessitatibus dolorem ab laudantium vel.
           </p>
         </div>
       </div>
-      <footer className="mt-8 text-gray-500 text-right">- {author}</footer>
+      <footer className="mt-8 text-gray-700 text-right">- {author}</footer>
     </blockquote>
   )
 }
@@ -73,8 +73,8 @@ export default function Detalle() {
 
   return (
     <>
-      <NavBar/>
-      <div className="relative max-w-screen-xl px-4 py-8 mx-auto">
+      <NavBar />
+      <div className="relative max-w-screen-2xl px-4 py-8 mx-auto">
         {/*<Link to="/home">
           <button
             className="text-pink-500 border border-pink-500 hover:bg-pink-500 hover:text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded-full outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
@@ -84,7 +84,7 @@ export default function Detalle() {
           </button>
         </Link>*/}
         <div>
-          <h1 className="text-2xl font-bold lg:text-3xl font-poiret-one">
+          <h1 className="text-3xl font-bold lg:text-5xl font-poiret-one">
             {libro.titulo}
           </h1>
           <p className="mt-1 text-sm text-gray-500 ">{libro.autor}</p>
@@ -200,19 +200,22 @@ export default function Detalle() {
           </div>
           <div className="lg:col-span-3">
             <div className="prose max-w-none ">
-              <h2 className="font-poiret-one font-bold ">Resumen:</h2>
-              <p>{libro.resumen}</p>
-              <h3 className="font-poiret-one font-bold ">Categorias</h3>
-              <ul>
-                <li>categorias asignadas</li>
-              </ul>
-              <div className="p-8 mx-auto max-w-screen-2xl sm:px-6 lg:px-8 shadow-2xl rounded-2xl bg-orange-50">
+              <div className="m-2 p-8 mx-auto max-w-screen-2xl sm:px-6 lg:px-8 shadow-2xl rounded-2xl bg-orange-50">
+                <h2 className="text-3xl font-poiret-one font-bold ">Resumen:</h2>
+                <p className='text-justify mr-2 ml-2'>{libro.resumen}</p>
+                <br />
+                <h3 className="text-2xl font-poiret-one font-bold ">Categorias</h3>
+                <ul>
+                  <li className='mr-2 ml-2'> - categorias asignadas</li>
+                </ul>
+              </div>
+              <div className="m-2 p-8 mx-auto max-w-screen-2xl sm:px-6 lg:px-8 shadow-2xl rounded-2xl bg-orange-50">
                 <div className="items-end justify-between sm:flex">
                   <div className="max-w-xl">
-                    <h2 className="text-4xl font-bold font-poiret-one tracking-tight sm:text-5xl">
+                    <h2 className="text-3xl font-bold font-poiret-one tracking-tight sm:text-3xl">
                       Reseñas
                     </h2>
-                    <p className="max-w-lg mt-8 text-gray-700">
+                    <p className="max-w-lg">
                       Ve lo que otros lectores tiene que decir
                     </p>
                   </div>
@@ -238,8 +241,10 @@ export default function Detalle() {
                   </a>
                 </div>
                 <div className="grid grid-cols-1 gap-4 mt-8 sm:grid-cols-2 lg:grid-cols-3">
-                  <ReviewCard title={'Brígido'} author={'Dylantero'} score={5} />
                   <ReviewCard title={'Waiting for more'} author={'Martin McFly'} score={5} />
+                  <ReviewCard title={'Espectacular'} author={'Anonimo'} score={5} />
+                  <ReviewCard title={'Pipí Cucú'} author={'Alberto Olmedo'} score={5} />
+                  <ReviewCard title={'Brígido'} author={'Dylantero'} score={5} />
                   <ReviewCard title={'Amazing read'} author={'Jhonny Test'} score={4} />
                   <ReviewCard title={'Great book'} author={'Eddie Murphy'} score={3} />
                 </div>
