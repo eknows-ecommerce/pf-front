36e60fa63c408971769227896fca0c6c56567304
AlexTRDev <104025084+AlexTRDev@users.noreply.github.com>
2022-08-10 01:18:33 -0500

    categorias filter front

--- src/components/FiltroCategorias/Categoria.jsx
+++ src/components/FiltroCategorias/Categoria.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-export default function Categoria({ id, nombre, handleClick }) {
+export default function Categoria({ id, nombre, handleClick, selected }) {
   return (
     <div className="flex items-center">
       <input
         id={id}
         type="checkbox"
         name={nombre}
-        onClick={() => handleClick(nombre)}
+        // onClick={handleClick}
         className="w-5 h-5 border-gray-300 rounded"
+        onChange={handleClick}
+        checked={selected}
       />
       <label htmlFor="toy" className="ml-3 text-sm font-medium">
         {nombre}
--- src/components/FiltroCategorias/Categorias.jsx
+++ src/components/FiltroCategorias/Categorias.jsx
@@ -1,12 +1,16 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAll } from '../../features/actions/libros'
-import { getAll as getAllCat, getById } from '../../features/actions/categorias'
+import { getAll as getAllCat } from '../../features/actions/categorias'
 import Categoria from './Categoria'
+import { setCategorias } from '../../features/reducers/librosSlice'
 
 export default function Categorias() {
-  const [catSelect, setCatSelect] = useState([])
+  const [selected, setSelected] = useState({})
+  // const [catSelect, setCatSelect] = useState([])
   const { categorias } = useSelector(({ categoriasStore }) => categoriasStore)
+
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -14,21 +18,35 @@ export default function Categorias() {
   }, [])
 
   function handleClick(e) {
-    setCatSelect([e])
+    setSelected({
+      ...selected,
+      [e.target.id]: e.target.checked,
+    })
   }
-  function handleButton() {
-    dispatch(getAll(`categoria=${catSelect[0]}`))
-  }
+
+  useEffect(() => {
+    let query = 'categorias='
+    for (const key in selected) {
+      if (selected[key]) {
+        query += key + ','
+      }
+    }
+    query = query.slice(0, -1)
+    dispatch(setCategorias(query))
+  }, [selected])
+
   return (
     <div className="px-5 py-6 space-y-2">
-      {categorias?.map((cat) => (
-        <Categoria
-          key={cat.id}
-          handleClick={handleClick}
-          nombre={cat.nombre}
-          id={cat.id}
-        />
-      ))}
+      {categorias &&
+        categorias?.map((cat) => (
+          <Categoria
+            key={crypto.randomUUID()}
+            nombre={cat.nombre}
+            id={cat.id}
+            handleClick={handleClick}
+            selected={selected[cat.id]}
+          />
+        ))}
     </div>
   )
 }
