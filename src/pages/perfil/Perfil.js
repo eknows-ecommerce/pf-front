f7ca6d6288912dcab08e808eea16ef8d18a8baf8
RichyMalicia <97110478+RichyMalicia@users.noreply.github.com>
2022-08-10 15:24:34 -0300

    perfil

--- src/pages/perfil/Perfil.js
+++ src/pages/perfil/Perfil.js
@@ -1,39 +1,34 @@
 import React, { useEffect } from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
 import { useDispatch, useSelector } from 'react-redux'
-import { getAllByEmail } from '../../features/actions/usuarios' //no existe, crear porfi
 import Button from '../../components/templates/Button'
 import { getById } from '../../features/actions/usuarios'
-import { useParams } from 'react-router-dom'
+
 
 export default function Perfil() {
   const dispatch = useDispatch()
-  const { id } = useParams()
-  console.log(id, 'id')
-  const usuariosData = useSelector(({ usuariosStore }) => usuariosStore)
-  console.log(usuariosData, 'user')
+  const {usuarios, usuario} = useSelector(({ usuariosStore }) => usuariosStore)
+  const idProf = usuarios[0][0].id
 
-  const { user, isLoading } = useAuth0()
   useEffect(() => {
-    dispatch(getAllByEmail(id))
-  }, [id, dispatch])
-  const perfil = usuariosData?.usuarios
-  const usu = perfil?.find((e) => e.email === user.email)
-  console.log(usu, 'usuarios')
-  console.log(perfil, 'perfil')
+    dispatch(getById(idProf))
+  }, [idProf, dispatch])
+  console.log("USUARIOS", usuarios);
+  console.log("USUARIO", usuario);
+  
+
   return (
     <div>
-      {perfil && perfil.length ? (
+     
         <article className="p-4 bg-gray-00 border border-gray-200 rounded-xl">
           <div className="flex items-center">
             <img
-              src={usu[0].picture}
-              alt={usu[0].name}
+              src={usuario.picture}
+              alt={usuario.name}
               className="w-20 h-20 rounded-full"
             />
             <div className="ml-3">
               <h5 className="text-lg font-medium text-violetapaleta">
-                {usu[0].name}
+                {usuario.name}
               </h5>
               <div className="flow-root">
                 <ul className="flex flex-wrap -m-1"></ul>
@@ -51,7 +46,7 @@ export default function Perfil() {
                   Pais
                 </h5>
                 <p className="mt-1 text-xs font-medium text-black-300">
-                  {usu[0].ciudad}
+                  {usuario.ciudad}
                 </p>
               </a>
             </li>
@@ -63,16 +58,14 @@ export default function Perfil() {
               >
                 <h5 className="font-medium text-violetapaleta">e-mail</h5>
                 <p className="mt-1 text-xs font-medium text-black">
-                  {usu[0].email}
+                  {usuario.email}
                 </p>
               </a>
             </li>
           </ul>
           <Button>Editar</Button>
         </article>
-      ) : (
-        { isLoading }
-      )}
+     
     </div>
   )
 }
