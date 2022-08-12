f1bf3acfb508f7bb4868858dce79c964dd7ebb13
MatiasCardullo <36178170+MatiasCardullo@users.noreply.github.com>
2022-08-12 20:06:55 -0300

    modal review

--- src/pages/detalle/Detalle.jsx
+++ src/pages/detalle/Detalle.jsx
@@ -10,6 +10,7 @@ export default function Detalle() {
   const dispatch = useDispatch()
   const { id } = useParams()
   const { libro } = useSelector(({ librosStore }) => librosStore)
+  const [showModal, setShowModal] = React.useState(false);
 
   useEffect(() => {
     dispatch(getById(id))
@@ -175,8 +176,66 @@ export default function Detalle() {
                       Ve lo que otros lectores tiene que decir
                     </p>
                   </div>
+                  <button
+                    className="inline-flex items-center flex-shrink-0 px-5 py-3 m-1 font-medium text-pink-600 border border-pink-600 rounded-full sm:mt-0 lg:mt-8 hover:bg-pink-600 hover:text-white"
+                    onClick={() => setShowModal(true)}
+                  >
+                    Escriba una review
+                  </button>
+                  {showModal ? (
+                    <>
+                      <div
+                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                      >
+                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                          {/*content*/}
+                          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                            {/*header*/}
+                            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                              <h3 className="text-3xl font-semibold">
+                                Modal Title
+                              </h3>
+                              <button
+                                className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                onClick={() => setShowModal(false)}
+                              >
+                                <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                  ×
+                                </span>
+                              </button>
+                            </div>
+                            {/*body*/}
+                            <div className="relative m-5 p-1 flex-auto items-center border-4 rounded-2xl">
+                              <input className="text-slate-800 text-lg leading-relaxed outline-none"
+                              placeholder='Escriba su review'
+                              >
+                              </input>
+                            </div>
+                            {/*footer*/}
+                            <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                              <button
+                                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                type="button"
+                                onClick={() => setShowModal(false)}
+                              >
+                                Close
+                              </button>
+                              <button
+                                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                type="button"
+                                onClick={() => setShowModal(false)}
+                              >
+                                Save Changes
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                    </>
+                  ) : null}
                   <a
-                    className="inline-flex items-center flex-shrink-0 px-5 py-3 mt-8 font-medium text-pink-600 border border-pink-600 rounded-full sm:mt-0 lg:mt-8 hover:bg-pink-600 hover:text-white"
+                    className="inline-flex items-center flex-shrink-0 px-5 py-3 m-1 font-medium text-pink-600 border border-pink-600 rounded-full sm:mt-0 lg:mt-8 hover:bg-pink-600 hover:text-white"
                     href="?"
                   >
                     Lea todas las reviews
