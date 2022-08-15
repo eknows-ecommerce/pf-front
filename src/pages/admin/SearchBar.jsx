7e41206a6b079712f47778ae197ecd288002794d
nnicolasg <104043118+nnicolasg@users.noreply.github.com>
2022-08-14 21:55:29 -0300

    search bar en panel de admin

--- src/pages/admin/SearchBar.jsx
+++ src/pages/admin/SearchBar.jsx
@@ -1,17 +1,16 @@
-function SearchBar() {
+export default function SearchBar() {
   return (
-    <div className="lg:max-w-[548px] w-full mx-auto border rounded-md">
+    <form
+      onSubmit={(e) => e.preventDefault()}
+      className="max-w-md px-4 mx-auto mt-12"
+    >
       <div>
         <input
-          className=" text-sm leading-none text-gray-600 bg-white  rounded  w-full px-10 py-4 outline-none"
           type="text"
-          name=""
-          id=""
           placeholder="Search"
+          className="w-full py-3 pl-5 pr-4 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-indigo-600"
         />
       </div>
-    </div>
+    </form>
   )
 }
-
-export default SearchBar
