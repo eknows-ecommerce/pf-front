30efbfccd298a0fd21a23135cb8cd1d0a96b4878
AlexTRDev <104025084+AlexTRDev@users.noreply.github.com>
2022-08-16 00:39:07 -0500

    Panel Admin - Pedidos - rutas - controller

--- src/features/actions/pedidos.js
+++ src/features/actions/pedidos.js
@@ -4,26 +4,30 @@ import axios from 'axios'
 const Production = process.env.NODE_ENV
 
 // <----------------- acciones que interactuan con la DB ----------------->
-export const getAll = createAsyncThunk('pedidos/@GET/ALL', async () => {
-  try {
-    const { data } = await axios.get(
-      Production === 'production'
-        ? `https://ebooks-back.herokuapp.com/pedido`
-        : 'http://localhost:8000/pedido'
-    )
-    return data
-  } catch (error) {
-    const msg = error.response.data.msg
-    return msg
+export const getAll = createAsyncThunk(
+  'pedidos/@GET/ALL',
+  async (query = '') => {
+    try {
+      console.log(query)
+      const { data } = await axios.get(
+        Production === 'production'
+          ? `https://ebooks-back.herokuapp.com/pedidos?${query}`
+          : `http://localhost:8000/pedidos?${query}`
+      )
+      return data
+    } catch (error) {
+      const msg = error.response.data.msg
+      return msg
+    }
   }
-})
+)
 
 export const getById = createAsyncThunk('pedido/@GETBYID', async (id) => {
   try {
     const { data } = await axios.get(
       Production === 'production'
-        ? `https://ebooks-back.herokuapp.com/pedido/${id}`
-        : `http://localhost:8000/pedido/${id}`
+        ? `https://ebooks-back.herokuapp.com/pedidos/${id}`
+        : `http://localhost:8000/pedidos/${id}`
     )
     return data
   } catch (error) {
@@ -36,8 +40,8 @@ export const create = createAsyncThunk('pedido/@CREATE', async (pedido) => {
   try {
     const { data } = await axios.post(
       Production === 'production'
-        ? `https://ebooks-back.herokuapp.com/pedido`
-        : 'http://localhost:8000/pedido',
+        ? `https://ebooks-back.herokuapp.com/pedidos`
+        : 'http://localhost:8000/pedidos',
       pedido
     )
     return data
@@ -47,30 +51,27 @@ export const create = createAsyncThunk('pedido/@CREATE', async (pedido) => {
   }
 })
 
-export const update = createAsyncThunk(
-  'pedido/@PUT/ID',
-  async ({ id, pedido }) => {
-    try {
-      const { data } = await axios.put(
-        Production === 'production'
-          ? `https://ebooks-back.herokuapp.com/pedido/${id}`
-          : `http://localhost:8000/pedido/${id}`,
-        pedido
-      )
-      return data
-    } catch (error) {
-      const msg = error.response.data.msg
-      return msg
-    }
+export const update = createAsyncThunk('pedido/@PUT/ID', async (pedido) => {
+  try {
+    const { data } = await axios.put(
+      Production === 'production'
+        ? `https://ebooks-back.herokuapp.com/pedidos/${pedido.id}`
+        : `http://localhost:8000/pedidos/${pedido.id}`,
+      pedido
+    )
+    return data
+  } catch (error) {
+    const msg = error.response.data.msg
+    return msg
   }
-)
+})
 
 export const deleteById = createAsyncThunk('pedido/@DELETE/ID', async (id) => {
   try {
     const { data } = await axios.delete(
       Production === 'production'
-        ? `https://ebooks-back.herokuapp.com/pedido/${id}`
-        : `http://localhost:8000/pedido/${id}`
+        ? `https://ebooks-back.herokuapp.com/pedidos/${id}`
+        : `http://localhost:8000/pedidos/${id}`
     )
     return data
   } catch (error) {
