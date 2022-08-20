6578b4c28e88b4cf8dd1c5933a5a82751900d180
RichyMalicia <97110478+RichyMalicia@users.noreply.github.com>
2022-08-20 01:08:12 -0300

    Corrección + vendidos
    Unas cositas ortográficas

--- src/components/NavBar/MasVendidos.jsx
+++ src/components/NavBar/MasVendidos.jsx
@@ -6,7 +6,7 @@ const MasVendidos = () => {
     <div className="2xl:mx-auto 2xl:container">
       <div className="lg:px-20 md:px-6 px-4 md:py-12 py-8">
         <h1 className="lg:text-4xl text-3xl font-semibold text-gray-800 text-center">
-          Descubre Nuestos Best Seller's
+          Descubre Nuestros Best Sellers
         </h1>
         <div className="flex justify-center">
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 lg:gap-8 lg:mt-16 md:mt-12 mt-8 items-center">
@@ -24,7 +24,7 @@ const MasVendidos = () => {
               <div className="absolute m-6 bottom-0 z-30">
                 <p className="text-sm leading-none text-white">J.K. Rowling</p>
                 <h1 className="w-64 text-2xl font-semibold leading-8 mt-2 text-white">
-                  Animales Fantasticos
+                  Animales Fantásticos
                 </h1>
                 <Link to={`/detalle/59`}>
                   <p className="mt-4 text-base font-medium cursor-pointer leading-4 underline text-white">
