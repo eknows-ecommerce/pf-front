6e79d826a6cd2acd1acc8e866a338588de7fcffa
RichyMalicia <97110478+RichyMalicia@users.noreply.github.com>
2022-08-24 14:54:18 -0300

    cambiosLoading

--- src/pages/detalle/Detalle.jsx
+++ src/pages/detalle/Detalle.jsx
@@ -6,7 +6,8 @@ import { getAll } from '../../features/actions/review'
 import Button from '../../components/templates/Button'
 import ReviewCard from '../../components/review/Review.jsx'
 import ReviewModal from '../../components/review/Write.jsx'
-import Footer from 'components/footer/Footer'
+/* import Footer from 'components/footer/Footer' */
+import Loading from 'components/loading/Loading'
 
 export default function Detalle() {
   const dispatch = useDispatch()
@@ -15,9 +16,12 @@ export default function Detalle() {
     JSON.parse(localStorage.getItem('carrito')) ?? []
   )
   const [revs, setRevs] = useState(6)
-  const { libros, libro } = useSelector(({ librosStore }) => librosStore)
+  const { libros, libro, cargando, cambiarCargando } = useSelector(
+    ({ librosStore }) => librosStore
+  )
   const { reviews } = useSelector(({ reviewsStore }) => reviewsStore)
   const { usuario } = useSelector(({ usuariosStore }) => usuariosStore)
+/*   const [loading = true, setLoading] = useState() */
   //const { pedido } = useSelector(({ pedidosStore }) => pedidosStore)
 
   let cats = []
@@ -39,11 +43,17 @@ export default function Detalle() {
       window.scroll(0, 0) // Restablece la posición de desplazamiento en la parte superior izquierda del documento
     }
     dispatch(getAll('?LibroId=' + id))
-  }, [])
+    return () => {
+      dispatch(getById())
+    }
+  }, [dispatch, id])
 
   useEffect(() => {
     dispatch(getById(id))
-  }, [])
+    return () => {
+      dispatch(getById())
+    }
+  }, [dispatch, id])
 
   function getReviews() {
     let out = []
@@ -80,120 +90,125 @@ export default function Detalle() {
   }*/
 
   return (
-    <>
-      <div className="relative max-w-screen-2xl px-4 py-8 mx-auto">
-        <div>
-          <h1 className="text-3xl font-bold lg:text-5xl font-comforta">
-            {libro.titulo}
-          </h1>
-          <p className="mt-1 text-sm text-gray-500 ">{libro.autor}</p>
-        </div>
-        <div className="grid gap-8 lg:items-start lg:grid-cols-4">
-          <div className="lg:col-span-3">
-            <img
-              alt=""
-              src={libro.portada}
-              className="relative mt-4 w-full rounded-xl h-72 lg:h-[540px] object-contain"
-            />
-            {/*extra imgs?*/}
+    <div>
+      {cargando === true ? (
+        <Loading cambiarCargando={cambiarCargando} />
+      ) : 
+        <div className="relative max-w-screen-2xl px-4 py-8 mx-auto">
+          <div>
+            <h1 className="text-3xl font-bold lg:text-5xl font-comforta">
+              {libro.titulo}
+            </h1>
+            <p className="mt-1 text-sm text-gray-500 ">{libro.autor}</p>
           </div>
-          <div className="lg:top-0 lg:sticky">
-            <form className="space-y-4 lg:pt-8">
-              {/** colores comentario abajo */}
-              <fieldset>
-                <legend className="text-lg font-bold">Tipo</legend>
-                <div className="flex mt-2 space-x-1">
-                  <label htmlFor="tipo_ebook" className="cursor-pointer">
-                    <input
-                      type="radio"
-                      id="tipo_ebook"
-                      name="tipo"
-                      className="sr-only peer"
-                      defaultChecked=""
-                    />
-                    <span className="block px-3 py-1 text-xs border border-gray-200 rounded-full peer-checked:bg-gray-100">
-                      eBook
-                    </span>
-                  </label>
-                  <label htmlFor="tipo_fisico" className="cursor-pointer">
-                    <input
-                      type="radio"
-                      id="tipo_fisico"
-                      name="tipo"
-                      className="sr-only peer"
-                      defaultChecked=""
-                    />
-                    <span className="block px-3 py-1 text-xs border border-gray-200 rounded-full peer-checked:bg-gray-100">
-                      Físico
-                    </span>
-                  </label>
-                </div>
-              </fieldset>
-              <div className="p-4 bg-gray-100 border rounded">
-                <p className="text-sm">
-                  <span className="block">Texto para posibles descuentos?</span>
-                  <a href="?" className="inline-block mt-1 underline">
-                    Find out more
-                  </a>
-                </p>
-              </div>
-              <div>
-                <p className="text-xl  font-bold">${libro.precio}</p>
-              </div>
-              <Link to="/home/carrito">
-                <Button onClick={handleCarrito}>Comprar</Button>
-              </Link>
-              <Button secondary>Agregar a favoritos</Button>
-            </form>
-          </div>
-
-          <div className="lg:col-span-3 prose max-w-none ">
-            <div className="m-2 p-8 mx-auto max-w-screen-2xl sm:px-6 lg:px-8 shadow-2xl rounded-2xl bg-orange-50">
-              <h2 className="text-3xl font-comforta-300 font-bold ">
-                Resumen:
-              </h2>
-              <p className="text-justify mr-2 ml-2">{libro.resumen}</p>
-              <br />
-              <div className="flex justify-evenly">
-                <div>
-                  <h3 className="text-2xl font-poiret-one font-bold">
-                    Categorias
-                  </h3>
-                  <ul>{cats}</ul>
-                </div>
-                <div>
-                  <h3 className="text-2xl font-poiret-one font-bold">Tags</h3>
-                  <ul>{tags}</ul>
-                </div>
-              </div>
+          <div className="grid gap-8 lg:items-start lg:grid-cols-4">
+            <div className="lg:col-span-3">
+              <img
+                alt=""
+                src={libro.portada}
+                className="relative mt-4 w-full rounded-xl h-72 lg:h-[540px] object-contain"
+              />
+              {/*extra imgs?*/}
             </div>
-            <div className="m-2 p-8 mx-auto max-w-screen-2xl sm:px-6 lg:px-8 shadow-2xl rounded-2xl bg-orange-50">
-              <div className="items-end justify-between sm:flex">
-                <>
-                  <div className="max-w-xl">
-                    <h2 className="text-3xl font-bold font-comforta-300 tracking-tight sm:text-3xl">
-                      Reseñas
-                    </h2>
-                    <p className="max-w-lg">
-                      Ve lo que otros lectores tiene que decir
-                    </p>
+            <div className="lg:top-0 lg:sticky">
+              <form className="space-y-4 lg:pt-8">
+                {/** colores comentario abajo */}
+                <fieldset>
+                  <legend className="text-lg font-bold">Tipo</legend>
+                  <div className="flex mt-2 space-x-1">
+                    <label htmlFor="tipo_ebook" className="cursor-pointer">
+                      <input
+                        type="radio"
+                        id="tipo_ebook"
+                        name="tipo"
+                        className="sr-only peer"
+                        defaultChecked=""
+                      />
+                      <span className="block px-3 py-1 text-xs border border-gray-200 rounded-full peer-checked:bg-gray-100">
+                        eBook
+                      </span>
+                    </label>
+                    <label htmlFor="tipo_fisico" className="cursor-pointer">
+                      <input
+                        type="radio"
+                        id="tipo_fisico"
+                        name="tipo"
+                        className="sr-only peer"
+                        defaultChecked=""
+                      />
+                      <span className="block px-3 py-1 text-xs border border-gray-200 rounded-full peer-checked:bg-gray-100">
+                        Físico
+                      </span>
+                    </label>
                   </div>
-                  {usuario.id > 0 ? (
-                    <ReviewModal idLibro={id} idUsuario={usuario.id} />
-                  ) : null}
-                  <button className="inline-flex items-center flex-shrink-0 px-5 py-3 m-1 font-medium text-pink-600 border border-pink-600 rounded-full sm:mt-0 lg:mt-8 hover:bg-pink-600 hover:text-white">
-                    Lea todas las reviews
-                  </button>
-                </>
+                </fieldset>
+                <div className="p-4 bg-gray-100 border rounded">
+                  <p className="text-sm">
+                    <span className="block">
+                      Texto para posibles descuentos?
+                    </span>
+                    <a href="?" className="inline-block mt-1 underline">
+                      Find out more
+                    </a>
+                  </p>
+                </div>
+                <div>
+                  <p className="text-xl  font-bold">${libro.precio}</p>
+                </div>
+                <Link to="/home/carrito">
+                  <Button onClick={handleCarrito}>Comprar</Button>
+                </Link>
+                <Button secondary>Agregar a favoritos</Button>
+              </form>
+            </div>
+
+            <div className="lg:col-span-3 prose max-w-none ">
+              <div className="m-2 p-8 mx-auto max-w-screen-2xl sm:px-6 lg:px-8 shadow-2xl rounded-2xl bg-orange-50">
+                <h2 className="text-3xl font-comforta-300 font-bold ">
+                  Resumen:
+                </h2>
+                <p className="text-justify mr-2 ml-2">{libro.resumen}</p>
+                <br />
+                <div className="flex justify-evenly">
+                  <div>
+                    <h3 className="text-2xl font-poiret-one font-bold">
+                      Categorias
+                    </h3>
+                    <ul>{cats}</ul>
+                  </div>
+                  <div>
+                    <h3 className="text-2xl font-poiret-one font-bold">Tags</h3>
+                    <ul>{tags}</ul>
+                  </div>
+                </div>
               </div>
-              <div className="grid grid-cols-1 gap-4 mt-8 sm:grid-cols-2 lg:grid-cols-3">
-                {getReviews()}
+              <div className="m-2 p-8 mx-auto max-w-screen-2xl sm:px-6 lg:px-8 shadow-2xl rounded-2xl bg-orange-50">
+                <div className="items-end justify-between sm:flex">
+                  <>
+                    <div className="max-w-xl">
+                      <h2 className="text-3xl font-bold font-comforta-300 tracking-tight sm:text-3xl">
+                        Reseñas
+                      </h2>
+                      <p className="max-w-lg">
+                        Ve lo que otros lectores tiene que decir
+                      </p>
+                    </div>
+                    {usuario.id > 0 ? (
+                      <ReviewModal idLibro={id} idUsuario={usuario.id} />
+                    ) : null}
+                    <button className="inline-flex items-center flex-shrink-0 px-5 py-3 m-1 font-medium text-pink-600 border border-pink-600 rounded-full sm:mt-0 lg:mt-8 hover:bg-pink-600 hover:text-white">
+                      Lea todas las reviews
+                    </button>
+                  </>
+                </div>
+                <div className="grid grid-cols-1 gap-4 mt-8 sm:grid-cols-2 lg:grid-cols-3">
+                  {getReviews()}
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <Footer />
-    </>
+      }
+    </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,26 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-
 import { useAuth0 } from '@auth0/auth0-react'
-
-
-import Loading from '../../components/loading/Loading'
-
-import Swal from 'sweetalert2'
-
-import CardLibro from 'components/cards/CardLibro'
+import Loading from 'components/loading/Loading'
+/* import CardLibro from 'components/cards/CardLibro' */
 import Paginacion from 'components/Paginacion/Paginacion'
 import Filtros from 'components/filtros/Filtros'
-
 import { getAll } from 'features/actions/libros'
 import { getByUser } from 'features/actions/favoritos'
-
 import { getByNickname } from 'features/actions/usuarios'
 
 import usePaginacion from 'hooks/usePaginacion'
 import Ordenamiento from 'components/filtros/Ordenamiento'
 import HomeLibros from 'components/contenedores/HomeLibros'
+import { cambiarCargando } from 'features/reducers/categoriasSlice'
 
 function Home() {
   const { user, isAuthenticated } = useAuth0()
@@ -36,10 +29,12 @@ function Home() {
     orden,
     buscarPor,
     limit,
+    cargando,
+    cambiarCargando
   } = useSelector(({ librosStore }) => librosStore)
 
   const { usuario } = useSelector(({ usuariosStore }) => usuariosStore)
-
+  
   const { paginado, handlePrevius, handleCurrent, handleNext, handleTotal } =
     usePaginacion()
 
@@ -86,13 +81,15 @@ function Home() {
     limit,
     paginado.currentPage,
   ])
-
+  
   return (
-    <section>
+    <div>
+      
+       <section>
       <div className="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:items-start">
           <Filtros handleCurrent={handleCurrent} />
-
+   
           <div className="lg:col-span-3 ">
             <div className="flex items-center justify-between bg-gray-100 px-2 z-20 rounded shadow-xl sticky lg:top-0 top-14">
               <p className="text-sm font-medium px-2 py-3">
@@ -143,16 +140,18 @@ function Home() {
 
             {paginado && (
               <Paginacion
-                paginaAnterior={handlePrevius}
-                paginaSiguiente={handleNext}
-                paginaSeleccionada={handleCurrent}
-                {...paginado}
+              paginaAnterior={handlePrevius}
+              paginaSiguiente={handleNext}
+              paginaSeleccionada={handleCurrent}
+              {...paginado}
               />
-            )}
+              )}
           </div>
+      
         </div>
       </div>
     </section>
+    </div>
   )
 }
 
