85b4ee17a27fd6a183408909024f4248a1c18043
alberto012 <rechavarria2@gmail.com>
2022-08-25 10:05:30 -0300

    sin load

--- src/components/NavBar/MejoresValorados.jsx
+++ src/components/NavBar/MejoresValorados.jsx
@@ -72,11 +72,7 @@ const MasVendidos = () => {
             ))}
         </div>
 
-        <div className=" flex justify-center items-center">
-          <button className=" hover:bg-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 bg-gray-800 py-5 md:px-16 md:w-auto w-full lg:mt-28 md:mt-12 mt-10 text-white font-medium text-base leading-4">
-            Load More
-          </button>
-        </div>
+      
       </div>
     </div>
   )
